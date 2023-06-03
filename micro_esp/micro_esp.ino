5502fd9545f48ec97aab2285c4c4dbb3d70cebdb
emrepkgzl <emrepekguzel@gmail.com>
2023-06-04 00:32:46 +0300

    Esp32 code update
    Arayüz kullanıcıdan limit değerlerini okumak üzere yeniden düzenlendi, alınan değerlerin uart vasıtasıyla stm32 gönderilmesi sağlanırken stm32 den gelen kombi aktiflik durumunun ullanıcıya gösterimi sağlandı.

--- micro_esp/micro_esp.ino
+++ micro_esp/micro_esp.ino
@@ -1,27 +1,30 @@
  #include <WiFi.h>  
  #include <SPI.h>              //Library for using SPI Communication 
+ #include <mcp2515.h>          //Library for using CAN Communication (https://github.com/autowp/arduino-mcp2515/)
+ #include <SoftwareSerial.h>
  #include <Adafruit_NeoPixel.h>
  #include <string>
+ 
  #define PIXELPIN  18
  #define NUMPIXELS 1
-   
-   
- uint8_t byte_counter = 0;
- uint8_t bit_counter = 0;
- uint8_t bit_counter_2 = 0;
- uint8_t last_digits = 0;
+ 
+ SoftwareSerial mySerial(17, 18); // RX, TX
+  
  bool flag = false;
- int get_data2;
+ int get_data2 = 0;
+ int get_data3 = 0;
+ int room_temp = 0;
+ bool combi_state = false;
  String check;
  String check2;
  uint8_t j = 0;
-
+   
  // Create the objects for server and client  
  WiFiServer server(80);  
  WiFiClient client;  
    
- const char* ssid   = "ESP32-CanBus-WebServer";// This is the SSID that ESP32 will broadcast  
- const char* password = "microver";     // password should be atleast 8 characters to make it work  
+ const char* ssid   = "Room temp manager";// This is the SSID that ESP32 will broadcast  
+ const char* password = "stm32stm32";     // password should be atleast 8 characters to make it work  
    
  // Create the global variable  
  String http;
@@ -34,25 +37,24 @@
   pixel.show();
   
   Serial.begin(115200);             //Begins Serial Communication at 9600 baudrate
-  Serial.print("Connecting to ");  
+  //Serial.print("Connecting to ");  
   Serial.println(ssid);  
    
   // Create the ESP32 access point  
   WiFi.softAP(ssid, password);  
    
-  Serial.println( "" );  
-  Serial.println( "WiFi AP is now running" );  
-  Serial.println( "IP address: " );  
-  Serial.println( WiFi.softAPIP() );  
+  //Serial.println( "" );  
+  //Serial.println( "WiFi AP is now running" );  
+  //Serial.println( "IP address: " );  
+  //Serial.println( WiFi.softAPIP() );  
    
   
   server.begin();                    // Start our ESP32 server  
-           
-  SPI.begin();                       //Begins SPI communication
-  
+
   delay(1000);
 
-  Serial.println("void setup done");
+
+  //Serial.println("void setup done");
   pixel.setPixelColor(0, pixel.Color(0, 25, 25));
   pixel.show();
  }  
@@ -60,13 +62,13 @@
  void loop(){  
     
   if ( client = server.available() ) {  // Checks if a new client tries to connect to our server  
-   Serial.println("New Client.");  
+   //Serial.println("New Client.");  
    String clientData = "";  
    while ( client.connected() ) {    // Wait until the client finish sending HTTP request  
     if ( client.available() ) {     // If there is a data,  
      char c = client.read();      //  read one character  
      http += c;            //  then parse it  
-     Serial.write(c);
+     //Serial.write(c);
      if (c == '\n') {         // If the character is carriage return,   
       ///////////////////////
       char get_data[2];
@@ -76,11 +78,38 @@
       {
           get_data[0] = http.charAt(16);
           get_data[1] = http.charAt(17);
-          Serial.print("DATA: ");
+          //Serial.print("DATA: ");
           get_data2 = get_data[0] - '0';
           get_data2 = get_data2 * 10;
           get_data2 += get_data[1] - '0';
-          Serial.println(get_data2);
+          //Serial.println(get_data2);
+      }
+      get_data[0] = 0;
+      get_data[1] = 0;
+      if(check == "?input2=")
+      {
+          get_data[0] = http.charAt(16);
+          get_data[1] = http.charAt(17);
+          //Serial.print("DATA: ");
+          get_data3 = get_data[0] - '0';
+          get_data3 = get_data3 * 10;
+          get_data3 += get_data[1] - '0';
+          //Serial.println(get_data3);
+      }
+
+      while(Serial.available())
+      {
+        pixel.setPixelColor(0, pixel.Color(25, 0, 0));
+          pixel.show();
+        int incoming_data = Serial.read();
+        if(incoming_data == 97)
+        {
+          combi_state = true;
+        }
+        else if(incoming_data == 98)
+        {
+          combi_state = false;
+        }
       }
       check == "";
      ///////////////////////    
@@ -89,6 +118,7 @@
        sendResponse();        //    perform the necessary action  
        
       updateWebpage();    
+      mySerial.write(0xAA);
       delay(300); //stands for refresh
   
        break;  
@@ -102,8 +132,8 @@
    }   
    http = "";  
    client.stop();            // Disconnect the client.  
-   Serial.println("Client disconnected.");  
-   Serial.println("");  
+   //Serial.println("Client disconnected.");  
+   //Serial.println("");  
   }  
  }  
    
@@ -125,99 +155,86 @@
     
   
 
-  //client.print("<hr>");
+  client.print("<hr>");
 
   if(get_data == true)
   {
-
       // Web Page Heading  
-      client.print("<body><h1>Room Thermostat Manager");  
+      client.print("<body><h1>Room stats");  
       //client.print(999, HEX);
       client.println("</h1>");
   
-      //client.println("<meta http-equiv=refresh content=0"); //refresh
-      /*for(int i = 1; i < 10; i++)
-      {
+      client.println("<meta http-equiv=refresh content=0"); //refresh
 
-    
-        switch (i) //DISPLAYING DATA
-        {
-          case 1: client.print("<p>");  
-                  client.print(i);  
-                  client.print("st data: "); break;
-          case 2: client.print("<p>");  
-                  client.print(i);  
-                  client.print("nd data: "); break;
-          case 3: client.print("<p>");  
-                  client.print(i);  
-                  client.print("rd data: "); break;
-          default:client.print("<p>");  
-                  client.print(i);  
-                  client.print("th data: "); break;
-        }
-    
-        //client.print(cbyte.byte);
-        client.println("</p>");
-        
-         
-        client.print("<hr>");      
-      }*/
+
+          if((get_data2 <= 27) && (get_data2 >= 20) && (get_data3 <= 28) && (get_data3 >= 21) && (get_data3 > get_data2))
+          {
+            client.print("<p>Lower limit: ");  
+            client.print(get_data2);  
+            client.print("C");
+            client.print("<p>Upper limit: ");  
+            client.print(get_data3);  
+            client.print("C");
+            client.print("<p>Combi state: "); 
+            if(combi_state)
+            {
+               client.print("on");
+            }
+            else
+            {
+              client.print("off");
+            }
+            //client.print(combi_state);  
+            
       
-      client.print("<hr>");
-      client.print("<p>Room temp: ");
-      client.print(5);
-      client.print("<hr>");
-      client.print("<p>Offset: ");
-      client.print(5);
-      client.print("<hr>");
-      client.print("<p>Upper limit: ");
-      client.print(5);
-      client.print("<hr>");
-      client.print("<p>Lower limit: ");
-      client.print(5);
-      client.print("<hr>");
-      client.print("<p>Combi state: ");
-      client.print(5);
-      client.print("<hr>");
-      client.println("</p>");
+            //client.print(cbyte.byte);
+            client.println("</p>");
+          
+           
+            client.print("<hr>");  
+          }    
+          else
+          {
+            client.print("<p>Please set the limits</p>");
+          }
+      
     
   }
 
     
   if(send_data == true)
   {
-    if(check2 == "?input1=" || check2 == "?input2=")
+    if(check2 == "?input1=")
       {
-        if(j < 1)//get_data2)
-        {
-          // Web Page Heading  
-          client.print("<body><h1>Configure your room settings</h1>");
-          j++;
+        
           check2 = "";
           
-            client.print("<form action=/get>Lower temp limit: ");  
-            //client.print(j);   
+            client.print("<form action=/get>Type your upper limit (21 - 28):  ");   
+                    
           client.println("<input type=number name=input2><input type=submit value=Apply></form><br>");
-        }
-        else
-        {
-          if(check2 == "?input2=")
-          {
-            client.println("<p>OK.<p>");
-            send_data = false;
-          }
-        }
+
+        
       }
+      else if(check2 == "?input2=")
+      {
+        send_data = false;
+        //client.println("<p><a href=\"/GET_DATA/on\"><button>Show stats</button></a></p>");
+
+        //mySerial.write(get_data2 + 100); //UARTTAN GONDER
+        Serial.print(get_data2 + 100);
+        delay(200);
+        //mySerial.write(get_data3 + 200);
+        Serial.print(get_data3 + 200);
+      }
+      
       else
       {
          // Web Page Heading  
-        client.print("<body><h1>Configure your room settings</h1>");  
+        client.print("<body><h1>Configure Settings</h1>");  
         
         //client.println("<label for=Name>Name:</label>");
         //client.print("<input type=text id=Name name=Name placeholder=Number of digits>");
-        client.println("<p>Please set your upper temp limit. If your room temp pass tis value your combi will be turned off.<p>"); 
-        client.println("<form action=/get>Upper limit: <input type=number name=input1><input type=submit value=Apply></form><br>");
-        client.println("<p><a href=\"/SEND_DATA/<%Eval(Name)%>\"><button>Next page</button></a></p>");
+        client.println("<form action=/get>Type your lower limit (20 - 27): <input type=number name=input1><input type=submit value=Apply></form><br>");
         client.println("<p><a href=\"/SEND_DATA/off\"><button>Cancel</button></a></p>");
       }
   }
@@ -226,16 +243,16 @@
 
   if(send_data == false)
   {
-    client.println("<p><a href=\"/SEND_DATA/on\"><button>Go to settings</button></a></p>");
+    client.println("<p><a href=\"/SEND_DATA/on\"><button>Configure settings</button></a></p>");
 
-    /*if(get_data == true)
+    if(get_data == true)
     {
-      client.println("<p><a href=\"/GET_DATA/off\"><button>Do not get data</button></a></p>");
+      //client.println("<p><a href=\"/GET_DATA/off\"><button>Do not get data</button></a></p>");
     }
     else
     {
-      client.println("<p><a href=\"/GET_DATA/on\"><button>Get data</button></a></p>");
-    }*/
+      client.println("<p><a href=\"/GET_DATA/on\"><button>Show stats</button></a></p>");
+    }
   }
   
   //BUTTON ACTS
